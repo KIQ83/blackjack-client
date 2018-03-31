9384de7a46cf2f1563add90bb4cf50df477a809c
Caique Moreira <caique.moreira@movile.com>
2018-03-31 17:07:58 -0300

    [FIX] fixed read of messages and fixed try/catch syntax

--- client.py
+++ client.py
@@ -47,32 +47,20 @@ def _send(socket, data):
 	print(data)
 	try:
 		serialized = json.dumps(data)
-	except (TypeError, ValueError), e:
+	except (TypeError, ValueError):
 		raise Exception('You can only send JSON-serializable data')
-	# send the length of the serialized data first
-	socket.send('%d\n' % len(serialized))
-	# send the serialized data
-	socket.sendall(serialized)
+	socket.send(serialized.encode())
 
 def _recv(socket):
 	# read the length of the data, letter by letter until we reach EOL
-	length_str = ''
-	char = socket.recv(1)
-	while char != '\n':
-		length_str += char
-	char = socket.recv(1)
-	total = int(length_str)
-	# use a memoryview to receive the data chunk by chunk efficiently
-	view = memoryview(bytearray(total))
-	next_offset = 0
-	while total - next_offset > 0:
-		recv_size = socket.recv_into(view[next_offset:], total - next_offset)
-	next_offset += recv_size
+	length = int(socket.recv(4))
+
+	messageInBytes = socket.recv(length)
+	deserialized = json.loads(fullMessage)
 	try:
-		deserialized = json.loads(view.tobytes())
-	except (TypeError, ValueError), e:
+		deserialized = json.loads(fullMessage)
+	except (TypeError, ValueError):
 		raise Exception('Data received was not in JSON format')
-	print(deserialized)
 	return deserialized
 
 class Player:
